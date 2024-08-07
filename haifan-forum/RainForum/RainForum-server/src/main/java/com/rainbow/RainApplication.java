9d355672ed098bb6cb3f20d6470501f8ed10a6b5
HaiFanNeon <haifan0312@163.com>
2024-08-07 21:51:51 +0800

    环境搭建

---
+++ haifan-forum/RainForum/RainForum-server/src/main/java/com/rainbow/RainApplication.java
@@ -0,0 +1,12 @@
+package com.rainbow;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RainApplication.class, args);
+    }
+}
