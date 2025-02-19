e924eda7baa25f8f54e0ac9720e36b0855edff69
HaiFanNeon <haifan0312@163.com>
2025-02-19 20:02:19 +0800

    configure

--- haifan-forum/RainForum/RainForum-common/src/main/java/com/rainbow/exception/LoginFailedException.java
+++
@@ -1,7 +0,0 @@
-package com.rainbow.exception;
-
-public class LoginFailedException extends BaseException{
-    public LoginFailedException(String msg){
-        super(msg);
-    }
-}
--- haifan-forum/RainForum/RainForum-common/src/main/java/com/rainbow/utils/MD5Util.java
+++
@@ -1,9 +0,0 @@
-package com.rainbow.utils;
-
-import org.springframework.util.DigestUtils;
-
-public class MD5Util {
-    public static String md5(String str) {
-        return DigestUtils.md5DigestAsHex(str.getBytes());
-    }
-}
